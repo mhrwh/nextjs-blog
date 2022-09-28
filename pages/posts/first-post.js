750e49e8a2f5ab51cb3f06801fe5c289a32edbe5
Ryutaro Matsumoto <matsumotoryutaro1@gmail.com>
2022-09-29 07:39:25 +0900

    refactor: Linkタグ修正

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -9,9 +9,7 @@ const FirstPost = () => {
       </Head>
       <h1>First Post</h1>
       <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
+        <Link href="/">Back to home</Link>
       </h2>
     </>
   )
