ae703ab70ccca982c278081b0b28a76e0e8d6fe6
Ryutaro Matsumoto <matsumotoryutaro1@gmail.com>
2022-09-29 23:26:09 +0900

    feat: 投稿ごとのページとルーティング

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Layout from '../../components/layout'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import Head from 'next/head'
+
+const Post = ({ postData }) => {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+
+export const getStaticPaths = async() => {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async({ params }) => {
+  // "await" キーワードを以下のように追加する
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default Post;
