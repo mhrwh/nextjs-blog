ea13a70a9d0ac3248ed6fa719e4d36950024b4d7
Ryutaro Matsumoto <matsumotoryutaro1@gmail.com>
2022-10-05 19:12:20 +0900

    feat: wordpress使えるように

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import { getAllPostIds, getPostData } from '../../lib/posts'
+//import { getAllPostIds, getPostData } from '../../lib/posts'
 import Layout from '../../components/layout'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
@@ -21,22 +21,52 @@ const Post = ({ postData }) => {
   )
 }
 
-export const getStaticPaths = async() => {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
+export async function getServerSideProps (context) {
+  try {
+      const host = 'wordpress'
+      const protocol = 'http'
+      const post = await fetch(`${protocol}://${host}/wp-json/wp/v2/posts/${context.query.id}`)
+          .then(data => data.json())
+      return {
+        props: {
+          postData: {
+            title: post.title.rendered,
+            date: post.date,
+            contentHtml: post.content.rendered,
+          }
+        }   
+          
+      }
+  } catch (e) {
+      console.log(e)
+      return {
+          props: {
+            postData: {
+              title: "error",
+              date: "error",
+              contentHtml: "error",
+            }
+          }
+      }
   }
 }
 
-export const getStaticProps = async({ params }) => {
-  // "await" キーワードを以下のように追加する
-  const postData = await getPostData(params.id)
-  return {
-    props: {
-      postData
-    }
-  }
-}
+// export const getStaticPaths = async() => {
+//   const paths = getAllPostIds()
+//   return {
+//     paths,
+//     fallback: false
+//   }
+// }
+
+// export const getStaticProps = async({ params }) => {
+//   // "await" キーワードを以下のように追加する
+//   const postData = await getPostData(params.id)
+//   return {
+//     props: {
+//       postData
+//     }
+//   }
+// }
 
 export default Post;
